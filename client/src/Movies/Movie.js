0a7baafae11a5e037f7f44d8784e33dde61e91a8
Juan C Hernandez <JChern83@users.noreply.github.com>
2020-06-17 16:14:27 -0700

    MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+
+  const { push } = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -13,18 +15,25 @@ function Movie({ addToSavedList }) {
       .then((res) => setMovie(res.data))
       .catch((err) => console.log(err.response));
   };
-
   const saveMovie = () => {
     addToSavedList(movie);
   };
-
   useEffect(() => {
     fetchMovie(params.id);
   }, [params.id]);
-
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
+
+  const removeMovie = (id) => {
+    console.log("Remove Function Is Running");
+    axios
+      .delete(`http://localhost:5000/api/movies/${id}`)
+      .then((res) => {
+        push("/");
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="save-wrapper">
@@ -33,8 +42,16 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <div
+        className="update-button"
+        onClick={() => push(`/update-movie/${params.id}`)}
+      >
+        Update
+      </div>
+      <div className="remove-button" onClick={() => removeMovie(params.id)}>
+        Remove
+      </div>
     </div>
   );
 }
-
 export default Movie;
